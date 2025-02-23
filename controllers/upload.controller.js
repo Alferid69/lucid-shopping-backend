c223e63941c1a6f3e6afaa8200890cdeaea467f9
Alferid69 <nsr-1452-14@amu.edu.et>
2025-02-23 22:19:14 +0300

    bug fix

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,22 +1,25 @@
-const express = require('express');
-const cloudinary = require('../utils/cloudinary');
-const upload = require('../middleware/multer');
+const express = require("express");
+const cloudinary = require("../utils/cloudinary");
+const upload = require("../middleware/multer");
+const User = require("../models/user.model");
 const router = express.Router();
 
-exports.uploadImage =  (req, res)=>{
-  cloudinary.uploader.upload(req.file.path, function(err, result){
-    if(err){
+exports.uploadImage = (req, res) => {
+  cloudinary.uploader.upload(req.file.path, async function (err, result) {
+    if (err) {
       console.log(err);
       res.status(400).json({
-        status: 'fail',
-        message: 'Error uploading file'
-      })
+        status: "fail",
+        message: "Error uploading file",
+      });
     }
 
     res.status(200).json({
-      status: 'success',
-      message: 'Image uploaded successfully',
-      data: result
-    })
-  })
-}
+      status: "success",
+      message: "Image uploaded successfully",
+      data: result,
+    });
+
+    await User.findByIdAndUpdate(req.user._id, { photo: result.url });
+  });
+};
