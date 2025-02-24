923eb4c0961ac4898eec6a8ba9255779947adead
Alferid69 <nsr-1452-14@amu.edu.et>
2025-02-24 16:11:46 +0300

    bug fix

--- routers/upload.router.js
+++ routers/upload.router.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const upload = require('../middleware/multer');
 const { uploadImage } = require('../controllers/upload.controller');
+const { protect } = require('../controllers/auth.controller');
 const router = express.Router();
 
+router.use(protect);
 router.post('/upload', upload.single('image'), uploadImage);
 
 module.exports = router;
