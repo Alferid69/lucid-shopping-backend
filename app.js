5971dffab98c32a8e910959073236f14d3938002
Alferid Hassen <nsr-1452-14@amu.edu.et>
2025-10-26 21:07:12 +0300

    fix: update placeholder in server.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const productsRouter = require("./routers/products.router");
 const orderRouter = require("./routers/orders.router");
 const customerRouter = require("./routers/users.router");
 const reviewRouter = require("./routers/review.router");
-const uploadRouter = require("./routers/upload.router")
+const uploadRouter = require("./routers/upload.router");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/error.controller");
 
@@ -59,7 +59,7 @@ app.use("/api/v1/products", productsRouter);
 app.use("/api/v1/orders", orderRouter);
 app.use("/api/v1/users", customerRouter);
 app.use("/api/v1/reviews", reviewRouter);
-app.use("/api/v1/upload", uploadRouter)
+app.use("/api/v1/upload", uploadRouter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
